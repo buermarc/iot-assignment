90de82f8118f4af42a1d4d3455cf6c6a5ab019ea
mulc <buermarc2@gmail.com>
2020-03-08 16:27:52 +0100

    Refactor and little additions

--- src/alerts/alert_service.py
+++ src/alerts/alert_service.py
@@ -6,8 +6,9 @@ class AlertService: # Service Reagiert auf Messwerte
 
     #  value:  Neuer Schwellwert als einfache Zahl in der Einheit cm wird hier übergeben
     def set_alert_threshold (self, value):
-        pass
+        self.treshhold = value;
 
     #  value:  Aktuell gemessener Wert (siehe DistanceSensor) wird übergeben und es wird ein Alarm gesendet
     def on_distance_threshold_passed(self, value):
+        # write to mqtt topic in config.json
         pass
--- src/start.py
+++ src/start.py
@@ -1,1 +1,3 @@
 # Start der Anwendung
+if __name__ == "__main__":
+    pass    
